78f4152ea130947f76c6ce64642c52dcc39cb35b
Scott Idler <scott.a.idler@gmail.com>
2017-04-29 13:33:57 -0700

    added stub for target

--- src/target.py
+++ src/target.py
@@ -5,6 +5,10 @@ import cli
 
 def entry_point(argv):
     args = cli.parse(argv)
+    if '<source>' in args:
+        print('source found')
+    else:
+        print('repl time')
     return 0
 
 def target(*args):
