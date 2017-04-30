0306c6379dcaf488bb9b6dbd7801fcc7f5f121f4
Scott Idler <scott.a.idler@gmail.com>
2017-04-30 00:51:14 -0700

    fixed up test cases

--- test/post/test_sota.py
+++ test/post/test_sota.py
@@ -2,6 +2,29 @@
 # -*- coding: utf-8 -*-
 
 import os
+import sys
+
+from utils.shell import call
+
+HELP = '''
+usage:
+    sota [options] [<source>]
+
+options:
+    -h --help     show this screen
+    --version     show version
+
+source:
+    text | file
+
+sota is state of the art
+'''
+
+sys.dont_write_bytecode = True
 
 def test_exists():
     assert os.path.exists('bin/sota')
+
+def test_help():
+    _, stdout, _ = call('./sota --help')
+    assert HELP.strip() == stdout.strip()
--- test/pre/test_target.py
+++ test/pre/test_target.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import os
+import sys
+
+sys.dont_write_bytecode = True
+
 def test_import():
     import target
