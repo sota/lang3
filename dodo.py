f62b85d0fa512298dda766db777e5c41a82e7832
Scott Idler <scott.a.idler@gmail.com>
2017-05-03 19:47:20 -0700

    converted all dict literals to dict calls

--- dodo.py
+++ dodo.py
@@ -64,11 +64,11 @@ def task_version():
     versionfiles = json.load(open(VERSION_JSON))
     for filename, contents in versionfiles.items():
         svw = SotaVersionWriter(filename, fmt(contents))
-        yield {
-            'name': filename,
-            'actions': [svw.update],
-            'uptodate': [svw.uptodate],
-        }
+        yield dict(
+            name=filename,
+            actions=[svw.update],
+            uptodate=[svw.uptodate],
+        )
 
 def is_initd():
     return all([call(fmt('git config --get submodule.{submod}.url'), throw=False)[1] for submod in SUBMODS])
@@ -77,23 +77,23 @@ def task_init():
     '''
     run git submodule init on submods
     '''
-    return {
-        'actions': ['git submodule init ' + ' '.join(SUBMODS)],
-        'targets': ['.git/config'],
-        'uptodate': [is_initd],
-    }
+    return dict(
+        actions=['git submodule init ' + ' '.join(SUBMODS)],
+        targets=['.git/config'],
+        uptodate=[is_initd],
+    )
 
 def task_submod():
     '''
     run git submodule update on submods
     '''
     for submod in SUBMODS:
-        yield {
-            'name': submod,
-            'file_dep': [DODO],
-            'task_dep': ['init'],
-            'actions': [fmt('git submodule update {submod}')],
-        }
+        yield dict(
+            name=submod,
+            file_dep=[DODO],
+            task_dep=['init'],
+            actions=[fmt('git submodule update {submod}')],
+        )
 
 def task_test():
     '''
@@ -148,63 +148,63 @@ def task_libcli():
     '''
     build so libary for use as sota's commandline interface
     '''
-    return {
-        'file_dep': [DODO] + rglob('src/cli/*.{h,c,cpp}'),
-        'task_dep': ['pre', 'submod:src/docopt'],
-        'actions': [
+    return dict(
+        file_dep=[DODO] + rglob('src/cli/*.{h,c,cpp}'),
+        task_dep=['pre', 'submod:src/docopt'],
+        actions=[
             fmt('cd src/cli && make -j {J}'),
             fmt('install -C -D src/cli/libcli.so {LIBDIR}/libcli.so'),
         ],
-        'targets': ['src/cli/test', fmt('{LIBDIR}/libcli.so')],
-        'clean': [clean_targets],
-    }
+        targets=['src/cli/test', fmt('{LIBDIR}/libcli.so')],
+        clean=[clean_targets],
+    )
 
 def task_colm():
     '''
     build colm binary for use in build
     '''
-    return {
-        'file_dep': [DODO],
-        'task_dep': ['pre', 'submod:src/colm'],
-        'actions': [
+    return dict(
+        file_dep=[DODO],
+        task_dep=['pre', 'submod:src/colm'],
+        actions=[
             'cd src/colm && autoreconf -f -i',
             fmt('cd src/colm && ./configure --prefix={REPOROOT}'),
             'cd src/colm && make && make install',
         ],
-        'targets': [COLM],
-        'clean': [clean_targets],
-    }
+        targets=[COLM],
+        clean=[clean_targets],
+    )
 
 def task_ragel():
     '''
     build ragel binary for use in build
     '''
-    return {
-        'file_dep': [DODO],
-        'task_dep': ['pre', 'submod:src/ragel', 'colm'],
-        'actions': [
+    return dict(
+        file_dep=[DODO],
+        task_dep=['pre', 'submod:src/ragel', 'colm'],
+        actions=[
             'cd src/ragel && autoreconf -f -i',
             fmt('cd src/ragel && ./configure --prefix={REPOROOT} --with-colm={REPOROOT} --disable-manual'),
             'cd src/ragel && make && make install',
         ],
-        'targets': [RAGEL],
-        'clean': [clean_targets],
-    }
+        targets=[RAGEL],
+        clean=[clean_targets],
+    )
 
 def task_liblexer():
     '''
     build so libary for use as sota's lexer
     '''
-    return {
-        'file_dep': [DODO] + rglob('src/lexer/*.{h,rl,c}'),
-        'task_dep': ['pre', 'ragel', 'version:src/cli/version.h'],
-        'actions': [
+    return dict(
+        file_dep=[DODO] + rglob('src/lexer/*.{h,rl,c}'),
+        task_dep=['pre', 'ragel', 'version:src/cli/version.h'],
+        actions=[
             fmt('cd src/lexer && LD_LIBRARY_PATH={REPOROOT}/lib make -j {J} RAGEL={REPOROOT}/{RAGEL}'),
             fmt('install -C -D src/lexer/liblexer.so {LIBDIR}/liblexer.so'),
         ],
-        'targets': ['src/lexer/lexer.cpp', 'src/lexer/test', fmt('{LIBDIR}/liblexer.so')],
-        'clean': [clean_targets],
-    }
+        targets=['src/lexer/lexer.cpp', 'src/lexer/test', fmt('{LIBDIR}/liblexer.so')],
+        clean=[clean_targets],
+    )
 
 def task_sota():
     '''
@@ -240,14 +240,14 @@ def task_tidy():
     '''
     clean submods and sota/lang repo
     '''
-    yield {
-        'name': 'sota/lang',
-        'actions': ['git clean -xfd'],
-    }
+    yield dict(
+        name='sota/lang',
+        actions=['git clean -xfd'],
+    )
     for submod in SUBMODS:
-        yield {
-            'name': submod,
-            'actions': [
+        yield dict(
+            name=submod,
+            actions=[
                 fmt('cd {submod} && git reset --hard HEAD && git clean -xfd')
             ],
-        }
+        )
